8e0f4bf3931d3335c084c33256fde9d62239e0fe
Ekaterina Petrochenkova <epetrochenkova001@yandex.ru>
2021-05-29 23:16:00 +0300

    Final ver.

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ with open("Cookbook.txt", "r", encoding="utf-8") as file:
             dish_ingr_list.append(ing_dict)
         file.readline()
         cook_book_dict[dish_name] = dish_ingr_list
-    print(cook_book_dict)
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
         for ing in cook_book_dict[dish]:
             ing_name = ing["ingredient_name"]
-            shop_list[ing_name] = { "quantity": int(ing["quantity"]) * person_count, "measure": ing["measure"]}
+            if ing_name not in shop_list:
+                shop_list[ing_name] = {"quantity": int(ing["quantity"]) * person_count, "measure": ing["measure"]}
+            else:
+                shop_list[ing_name]["quantity"] += int(ing["quantity"]) * person_count
     return shop_list
 
-print(get_shop_list_by_dishes(["Омлет", "Фахитос"], 2))
+print(cook_book_dict)
+print(get_shop_list_by_dishes(["Омлет", "Фахитос"], 3))
+
 
